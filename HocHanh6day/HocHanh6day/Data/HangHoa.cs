8e2e43557630ba64ff65a9f7fac435b1fdfcd9c7
trongnghia <maivantrongnghia64@gmail.com>
2023-05-30 15:58:32 +0700

    Fluent API

--- HocHanh6day/HocHanh6day/Data/HangHoa.cs
+++ HocHanh6day/HocHanh6day/Data/HangHoa.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace HocHanh6day.Data
 {
@@ -23,6 +24,13 @@ namespace HocHanh6day.Data
         public int? MaLoai { get; set; }
         [ForeignKey("MaLoai")]
         public Loai Loai { get; set; }
+
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+
+        public HangHoa()
+        {
+            DonHangChiTiets = new HashSet<DonHangChiTiet>();
+        }
 
     }
 }
--- HocHanh6day/HocHanh6day/Data/MyDbContext.cs
+++ HocHanh6day/HocHanh6day/Data/MyDbContext.cs
@@ -11,5 +11,42 @@ namespace HocHanh6day.Data
 
         public DbSet<HangHoa> HangHoas { get; set; }
         public DbSet<Loai> Loais { get; set; }
+
+        public DbSet<DonHang> DonHangs { get; set; }
+
+        public DbSet<DonHangChiTiet> DonHangChiTiets { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DonHang>(e =>
+            {
+                e.ToTable("DonHang");
+                e.HasKey(dh => dh.MaDh);
+                e.Property(dh => dh.NgayDat).HasDefaultValueSql("getutcdate()");
+                e.Property(dh => dh.NguoiNhan).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<DonHangChiTiet>(e =>
+            {
+                e.ToTable("ChiTietDonHang");
+                e.HasKey(e => new
+                {
+                    e.MaDh,
+                    e.MaHh
+                });
+
+                e.HasOne(e => e.DonHang)
+                    .WithMany(e => e.DonHangChiTiets)
+                    .HasForeignKey(e => e.MaDh)
+                    .HasConstraintName("FK_DonHangChiTiet_DonHang");
+
+                e.HasOne(e => e.HangHoa)
+                    .WithMany(e => e.DonHangChiTiets)
+                    .HasForeignKey(e => e.MaHh)
+                    .HasConstraintName("FK_DonHangChiTiet_HangHoa");
+
+            });
+        }
     }
 }
